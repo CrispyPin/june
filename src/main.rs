e677710831390490620ff2e540de08234d1778c8
CrispyPin <crispin@tasa.se>
2022-10-01 20:12:34 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use chrono::{Datelike, Local, NaiveDate};
+use std::env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	if args.len() > 1 {
+		let date = NaiveDate::parse_from_str(&args[1], "%Y-%m-%d")
+			.expect("Input date must follow format 2022-06-01");
+		juneth(date);
+	} else {
+		let now = Local::now().date().naive_local();
+		juneth(now);
+	}
+}
+
+fn juneth(now: NaiveDate) {
+	let last_june_1st = if now.month() > 6 {
+		NaiveDate::from_ymd(now.year(), 6, 1)
+	} else {
+		NaiveDate::from_ymd(now.year() - 1, 6, 1)
+	};
+	let juneth = now.num_days_from_ce() - last_june_1st.num_days_from_ce() + 1;
+	println!("June {juneth}th");
+}
