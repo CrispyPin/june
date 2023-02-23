8825c68eb195024aff6bfe7fe50a2fc61d67a524
Erik Magnusson <erik.arn.magnusson@gmail.com>
2023-02-23 15:39:39 +0100

    expression simplification

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use chrono::{Datelike, Local, NaiveDate};
 use std::{env, process::exit};
 
 fn main() {
-	let date = if let Some(arg) = env::args().nth(1) {
+	juneth(if let Some(arg) = env::args().nth(1) {
 		NaiveDate::parse_from_str(&arg, "%Y-%m-%d").unwrap_or_else(|err| {
 			eprintln!(
 				"ERROR: {err} > \"{}\" \n\
@@ -12,20 +12,13 @@ fn main() {
 			exit(-1);
 		})
 	} else {
-		Local::now().date().naive_local()
-	};
-
-	juneth(date);
+		Local::now().date_naive()
+	});
 }
 
 fn juneth(now: NaiveDate) {
-	let last_june_1st = if now.month() >= 6 {
-		NaiveDate::from_ymd(now.year(), 6, 1)
-	} else {
-		NaiveDate::from_ymd(now.year() - 1, 6, 1)
-	};
-
-	let juneth = now.num_days_from_ce() - last_june_1st.num_days_from_ce() + 1;
+	let last_june_1st = NaiveDate::from_ymd(now.year() - i32::from(now.month() < 6), 6, 1);
+	let juneth = (now - last_june_1st).num_days() + 1;
 	let ending = match juneth % 10 {
 		1 => "st",
 		2 => "nd",
